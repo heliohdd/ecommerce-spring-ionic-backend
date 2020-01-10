4a077e13462a317817822de5c36b6cd6f7b7a0a3
Helio Dourado <heliohdd@yahoo.com.br>
2020-01-10 18:55:43 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/CategoriaService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.heliohdd.spring_boot_ionic_backend.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos!");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}	
 
 }
