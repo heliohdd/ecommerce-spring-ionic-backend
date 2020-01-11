5c290c37dd5fdce04d2040a305e5c7c399b47eec
Helio Dourado <heliohdd@yahoo.com.br>
2020-01-11 10:25:12 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/CategoriaService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.heliohdd.spring_boot_ionic_backend.domain.Categoria;
+import com.heliohdd.spring_boot_ionic_backend.dto.CategoriaDTO;
 import com.heliohdd.spring_boot_ionic_backend.repositories.CategoriaRepository;
 import com.heliohdd.spring_boot_ionic_backend.services.exception.DataIntegrityException;
 import com.heliohdd.spring_boot_ionic_backend.services.exception.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
