7fea41e4f8c42438c4c689b8c31924ffeadb9247
Helio Dourado <heliohdd@yahoo.com.br>
2020-01-13 14:57:58 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/resources/ClienteResource.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.heliohdd.spring_boot_ionic_backend.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
 import com.heliohdd.spring_boot_ionic_backend.dto.ClienteDTO;
+import com.heliohdd.spring_boot_ionic_backend.dto.ClienteNewDTO;
 import com.heliohdd.spring_boot_ionic_backend.services.ClienteService;
 
 @RestController
@@ -31,7 +34,15 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto); 
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/ClienteService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.heliohdd.spring_boot_ionic_backend.domain.Cidade;
 import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
+import com.heliohdd.spring_boot_ionic_backend.domain.Endereco;
+import com.heliohdd.spring_boot_ionic_backend.domain.enums.TipoCliente;
 import com.heliohdd.spring_boot_ionic_backend.dto.ClienteDTO;
+import com.heliohdd.spring_boot_ionic_backend.dto.ClienteNewDTO;
 import com.heliohdd.spring_boot_ionic_backend.repositories.ClienteRepository;
+import com.heliohdd.spring_boot_ionic_backend.repositories.EnderecoRepository;
 import com.heliohdd.spring_boot_ionic_backend.services.exception.DataIntegrityException;
 import com.heliohdd.spring_boot_ionic_backend.services.exception.ObjectNotFoundException;
 
@@ -21,11 +26,21 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -55,6 +70,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
