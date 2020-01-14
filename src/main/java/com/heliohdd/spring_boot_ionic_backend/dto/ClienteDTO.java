5fbb03028a061e7628f622bac22f20778da6a27e
Helio Dourado <heliohdd@yahoo.com.br>
2020-01-14 18:23:10 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/dto/ClienteDTO.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
+import com.heliohdd.spring_boot_ionic_backend.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/validation/ClienteUpdateValidator.java
@@ -2,45 +2,45 @@ package com.heliohdd.spring_boot_ionic_backend.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
-import com.heliohdd.spring_boot_ionic_backend.domain.enums.TipoCliente;
-import com.heliohdd.spring_boot_ionic_backend.dto.ClienteNewDTO;
+import com.heliohdd.spring_boot_ionic_backend.dto.ClienteDTO;
 import com.heliohdd.spring_boot_ionic_backend.repositories.ClienteRepository;
 import com.heliohdd.spring_boot_ionic_backend.resources.exception.FieldMessage;
-import com.heliohdd.spring_boot_ionic_backend.services.validation.utils.BR;
 
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
 
+	@Autowired
+	private HttpServletRequest request;
+	
 	@Autowired
 	private ClienteRepository repo;
 	
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
-//			inclua os testes aqui, inserindo erros na lista
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
-		}
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
-		}
-		
 		Cliente aux = repo.findByEmail(objDto.getEmail());
-		if(aux != null) {
+		if(aux != null && !aux.getId().equals(uriId)) {
 			list.add(new FieldMessage("Email", "Email já existente"));
 		}
 		
