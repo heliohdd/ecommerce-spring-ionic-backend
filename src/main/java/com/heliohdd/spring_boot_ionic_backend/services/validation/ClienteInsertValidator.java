b2e58e57c9a93e3bad97887d385ac4708444dcbf
Helio Dourado <heliohdd@yahoo.com.br>
2020-01-14 17:44:19 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/validation/ClienteInsertValidator.java
@@ -6,14 +6,22 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
 import com.heliohdd.spring_boot_ionic_backend.domain.enums.TipoCliente;
 import com.heliohdd.spring_boot_ionic_backend.dto.ClienteNewDTO;
+import com.heliohdd.spring_boot_ionic_backend.repositories.ClienteRepository;
 import com.heliohdd.spring_boot_ionic_backend.resources.exception.FieldMessage;
 import com.heliohdd.spring_boot_ionic_backend.services.validation.utils.BR;
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("Email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
