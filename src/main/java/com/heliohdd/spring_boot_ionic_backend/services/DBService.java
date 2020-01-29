f430a07565736e76df44e60a3adf30fbe691ebde
Helio Dourado <heliohdd@yahoo.com.br>
2020-01-29 09:45:04 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/DBService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/DBService.java
@@ -20,6 +20,7 @@ import com.heliohdd.spring_boot_ionic_backend.domain.PagamentoComCartao;
 import com.heliohdd.spring_boot_ionic_backend.domain.Pedido;
 import com.heliohdd.spring_boot_ionic_backend.domain.Produto;
 import com.heliohdd.spring_boot_ionic_backend.domain.enums.EstadoPagamento;
+import com.heliohdd.spring_boot_ionic_backend.domain.enums.Perfil;
 import com.heliohdd.spring_boot_ionic_backend.domain.enums.TipoCliente;
 import com.heliohdd.spring_boot_ionic_backend.repositories.CategoriaRepository;
 import com.heliohdd.spring_boot_ionic_backend.repositories.CidadeRepository;
@@ -116,14 +117,20 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "testemailspringboot@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "heliohdd@yahoo.com.br", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
