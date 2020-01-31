dcb0f968fdd0cb309f9839368c0214871f033c8f
Helio Dourado <heliohdd@yahoo.com.br>
2020-01-30 23:08:08 -0300

    Implementando autorizacao

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/config/SecurityConfig.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.heliohdd.spring_boot_ionic_backend.security.JWTAuthenticationFilter;
+import com.heliohdd.spring_boot_ionic_backend.security.JWTAuthorizationFilter;
 import com.heliohdd.spring_boot_ionic_backend.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
