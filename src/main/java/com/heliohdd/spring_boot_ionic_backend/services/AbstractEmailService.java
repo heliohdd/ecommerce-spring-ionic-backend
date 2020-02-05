d8e851eaf8592f0a332bd35f4a4195df32166fda
Helio Dourado <heliohdd@yahoo.com.br>
2020-02-05 11:40:26 -0300

    Esqueci a senha

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/AbstractEmailService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
 import com.heliohdd.spring_boot_ionic_backend.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -39,6 +40,22 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
+		return sm;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+	
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
 		return sm;
 	}
 	
--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/EmailService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
 import com.heliohdd.spring_boot_ionic_backend.domain.Pedido;
 
 public interface EmailService {
@@ -11,6 +12,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
