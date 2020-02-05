8586d1704a584ad424779d73e0bc27aa7ee9ba2d
Helio Dourado <heliohdd@yahoo.com.br>
2020-02-04 22:13:30 -0300

    Refresh token

---
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.heliohdd.spring_boot_ionic_backend.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.heliohdd.spring_boot_ionic_backend.security.JWTUtil;
+import com.heliohdd.spring_boot_ionic_backend.security.UserSS;
+import com.heliohdd.spring_boot_ionic_backend.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
