9d9539bf2375113cf17ea6aeba6a2c8043aa6345
Helio Dourado <heliohdd@yahoo.com.br>
2020-02-07 10:53:00 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/SpringBootIonicBackendApplication.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/SpringBootIonicBackendApplication.java
@@ -1,15 +1,17 @@
 package com.heliohdd.spring_boot_ionic_backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+import com.heliohdd.spring_boot_ionic_backend.services.S3Service;
 
 @SpringBootApplication
 public class SpringBootIonicBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIonicBackendApplication.class, args);
@@ -17,6 +19,6 @@ public class SpringBootIonicBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {		
-
+		s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
 	}	
 }
