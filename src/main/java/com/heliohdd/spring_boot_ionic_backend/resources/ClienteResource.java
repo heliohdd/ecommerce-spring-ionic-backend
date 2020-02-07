cbe90991b6d21a3962d1136b06c8a91a7cf6f373
Helio Dourado <heliohdd@yahoo.com.br>
2020-02-07 12:06:28 -0300

    Enviando imagem via endpoint

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/resources/ClienteResource.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
@@ -81,4 +82,9 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
+	@RequestMapping(value="/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
+	}
 }
--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/ClienteService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.heliohdd.spring_boot_ionic_backend.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.heliohdd.spring_boot_ionic_backend.domain.Cidade;
 import com.heliohdd.spring_boot_ionic_backend.domain.Cliente;
@@ -36,6 +38,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		
@@ -103,5 +108,9 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
