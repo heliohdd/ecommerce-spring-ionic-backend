c1f426e025f3904906027b33b74780c74472258b
Helio Dourado <heliohdd@yahoo.com.br>
2020-02-07 18:03:29 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/heliohdd/spring_boot_ionic_backend/services/ClienteService.java
+++ src/main/java/com/heliohdd/spring_boot_ionic_backend/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.heliohdd.spring_boot_ionic_backend.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 
 	public Cliente find(Integer id) {
 		
@@ -118,12 +126,8 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 
-		URI uri = s3Service.uploadFile(multipartFile);
-		
-		Cliente cli = find(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-		
-		return uri;
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
